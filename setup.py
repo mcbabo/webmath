35471a7b208b663c4ee929a11d66cbf900bf7cdd
Moritz <67439212+mcbabo@users.noreply.github.com>
2021-11-06 15:17:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-# requirements = ["aiohttp >= 3.6.0", "urllib3"]
-requirements = []
-
-with open("requirements.txt", "r") as f:
-  requirements = f.read().splitlines()
-  
 # See note below for more information about classifiers
 classifiers = [
     "Development Status :: 5 - Production/Stable",
@@ -17,16 +11,18 @@ classifiers = [
 
 setup(
     name="web_math",
-    version="1.0.14",
+    version="1.1.0",
     description="For online math api",
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
     url="https://github.com/mcbabo/webmath",
     author="Moritz Joksch",
-    license="MIT",  # note the American spelling
+    license="MIT",
     classifiers=classifiers,
-    keywords="discord discord-calc calculator easy-calc simple-calculator discord.py pycord python math",  # used when people are searching for a module, keywords separated with a space
+    keywords="discord discord-calc calculator easy-calc simple-calculator discord.py pycord python math",
     packages=find_packages(),
-    install_requires=requirements,  # a list of other Python modules which this module depends on.  For example RPi.GPIO
+    install_requires=[
+        "aiohttp", "urllib3"
+    ],
     include_package_data=True
 )
